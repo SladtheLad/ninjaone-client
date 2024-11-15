6e3eb39e1e330a9b0ace711efedb77ced19e2113
Nick Sladden <nickoliasxii@gmail.com>
2024-11-14 18:04:39 -0600

    setup and header

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import ninjaOneLogo from "/NinjaOneLogo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <header>
+                <img src={ninjaOneLogo} className="logo" alt="Ninja One Logo" />
+            </header>
+        </>
+    );
+}
+
+export default App;
