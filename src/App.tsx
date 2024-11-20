f15dc1efaf90d96e71d19b8053cb15cbf195107c
Nick Sladden <nickoliasxii@gmail.com>
2024-11-20 11:10:09 -0600

    add .env for swapping URI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import ninjaOneLogo from "/NinjaOneLogo.svg";
 import "./App.css";
 
+console.log(import.meta.env.VITE_BASE_SERVICES_URI);
+
 function App() {
-    return (
-        <>
-            <header>
-                <img src={ninjaOneLogo} className="logo" alt="Ninja One Logo" />
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header>
+        <img src={ninjaOneLogo} className="logo" alt="Ninja One Logo" />
+      </header>
+    </>
+  );
 }
 
 export default App;
